5f2a97062fe13d14364b6b02f505f9732e50f31d
quenn <75434427+amalie11@users.noreply.github.com>
2022-08-25 22:49:15 -0400

    Add files via upload

---
+++ pdf_textgrabber/main.py
@@ -0,0 +1,54 @@
+import os,sys
+import pdf2image
+try:
+    from PIL import Image
+except ImportError:
+    import Image 
+import pytesseract
+
+path="Enter path: "
+i=1
+def delete_ppms():
+  for file in os.listdir(PATH):
+    if '.ppm' in file or '.DS_Store' in file:
+      try:
+          os.remove(PATH + file)
+      except FileNotFoundError:
+          pass
+df_files = []
+docx_files = []
+
+# this is to append the document names into the lists by their extension type
+for f in os.listdir(PATH):
+  full_name = os.path.join(PATH, f) 
+  if os.path.isfile(full_name):
+    name = os.path.basename(f)
+    filename, ext = os.path.splitext(name)
+    if ext == '.pdf':
+      pdf_files.append(name)
+    elif ext == ('.docx'):
+      docx_files.append(name)
+
+def pdf_extract(file, i):
+  print("extracting from file:", file)
+  delete_ppms()
+  images = pdf2image.convert_from_path(PATH + file, output_folder=PATH)
+  j = 0
+  for file in sorted (os.listdir(PATH)):
+      if '.ppm' in file and 'image' not in file:
+        os.rename(PATH + file, PATH + 'image' + str(i) + '-' + str(j) + '.ppm')
+        j += 1
+  j = 0
+  f = open(PATH +'result{}.txt'.format(i), 'w')
+  files = [f for f in os.listdir(PATH) if '.ppm' in f]
+
+  for file in sorted(files, key=lambda x: int(x[x.index('-') + 1: x.index('.')])):
+      temp = pytesseract.image_to_string(Image.open(PATH + file))
+      f.write(temp)
+  f.close()
+
+  for i in range(len(pdf_files)):
+  pdf_file = pdf_files[i]
+  pdf_extract(pdf_file, i)
+ 
+  #come back to this later
